06e0516fe1e8263f0592bfab953a3d93084cdf28
DavidBasarab <dbasarab@haivision.com>
2024-01-22 12:02:29 -0500

    can run

--- src/TcpFake/Program.cs
+++ src/TcpFake/Program.cs
@@ -1,2 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Reflection;
+using FatCat.Toolkit.Console;
+using FatCat.Toolkit.Web.Api;
+using FatCat.Toolkit.WebServer;
+
+namespace FatCat.TcpFake;
+
+public static class Program
+{
+    private static ToolkitWebApplicationSettings applicationSettings;
+
+    public static void Main(params string[] args)
+    {
+        applicationSettings = new ToolkitWebApplicationSettings
+        {
+            Options = WebApplicationOptions.CommonOptions,
+            ContainerAssemblies =
+            [
+                Assembly.GetExecutingAssembly(),
+                typeof(ToolkitWebApplication).Assembly
+            ],
+            OnWebApplicationStarted = Started,
+            Args = args
+        };
+
+        ToolkitWebApplication.Run(applicationSettings);
+    }
+
+    private static void Started()
+    {
+        try
+        {
+            ConsoleLog.WriteDarkCyan("Application has started");
+        }
+        catch (Exception e)
+        {
+            ConsoleLog.WriteException(e);
+        }
+    }
+}
