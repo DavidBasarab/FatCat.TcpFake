80bed0c2f3ce9d50b8c872c0e9c7c5152f4a19e5
DavidBasarab <dbasarab@haivision.com>
2024-01-24 10:52:12 -0500

    test api endpoint

---
+++ src/TcpFake/TestingEndpoint.cs
@@ -0,0 +1,17 @@
+﻿using FatCat.Toolkit.Console;
+using FatCat.Toolkit.Web;
+using FatCat.Toolkit.WebServer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FatCat.TcpFake;
+
+public class TestingEndpoint : Endpoint
+{
+    [HttpGet("test")]
+    public WebResult DoTest()
+    {
+        ConsoleLog.WriteDarkCyan("Hit test endpoint");
+
+        return Ok($"ACK | {DateTime.Now:h:mm:ss:fff tt}");
+    }
+}
